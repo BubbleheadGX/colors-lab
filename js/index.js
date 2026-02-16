fcfa78c1741749da9f1247c6798d2e984d6acf3c
Christopher Zavala <76709102+BubbleheadGX@users.noreply.github.com>
2026-02-15 20:08:29 -0500

    Add doLogout()

--- js/index.js
+++ js/index.js
@@ -98,3 +98,12 @@ function readCookie()
 //		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
 	}
 }
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
